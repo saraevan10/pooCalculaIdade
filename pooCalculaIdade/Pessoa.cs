09a318f9280b03bd5419125d9f0d4fb68cfa5cd4
saraevan10 <79640609+saraevan10@users.noreply.github.com>
2025-07-18 10:29:40 -0400

    Adicionar arquivos de projeto.

---
+++ pooCalculaIdade/Pessoa.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pooCalculaIdade
+{
+    public class Pessoa
+    {
+
+		public Pessoa(String nomePessoa, int anoNascimentoPessoa)  //Construtor
+		{
+			this.Nome = nomePessoa;
+			this.AnoNascimento = anoNascimentoPessoa;
+			this.CalcularIdade();
+		}
+
+
+		private int anoNascimento;  //variavel
+
+		public int AnoNascimento  //propriedade
+		{
+			get { return this.anoNascimento;; }
+			set { this.anoNascimento = value; }
+		}
+
+		private String nome; //variavel
+
+		public String Nome //propriedade
+		{
+			get { return this.nome; }
+			set { this.nome = value.ToUpper(); }
+		}
+
+		//Vamos melhorar
+		private int idade; //variavel
+
+		public int Idade  //propriedade
+		{
+			get {
+				this.CalcularIdade();
+				return this.idade; 
+			} //pega o valor da propriedade e entrega para quem solicitar
+		}
+
+
+		public void ExibirDados() //método
+		{
+			Console.WriteLine("Nome: " + this.nome);
+			Console.WriteLine("Ano de nascimento: " + this.AnoNascimento);
+			this.CalcularIdade();
+			Console.WriteLine("Idade: " + this.Idade);
+		}
+
+		private void CalcularIdade() // método
+		{
+			DateTime data = DateTime.Now;
+			int ano = data.Year;
+			this.idade = ano - this.AnoNascimento;
+		}
+	}
+}
---
+++ pooCalculaIdade/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pooCalculaIdade
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programa que calcula a idade de uma pessoa");           
+            Console.Write("Nome da pessoa: ");
+            String nome = Console.ReadLine();
+            Console.Write("Ano de nascimento: ");
+            int ano = Convert.ToInt32(Console.ReadLine());
+            Pessoa p;
+            p = new Pessoa(nome, ano);
+            p.ExibirDados();
+            Console.ReadKey();
+        }
+    }
+}
